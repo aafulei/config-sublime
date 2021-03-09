2fc53a8c4b5ce108d440d976e6eacecebd5bee9a
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-03-10 00:19:48 +0800

    10

--- My Own Commands/my-own-commands.py
+++ My Own Commands/my-own-commands.py
@@ -633,3 +633,19 @@ class NewFileListener(sublime_plugin.EventListener):
         except AttributeError:
             print("typeof=", type(view.window()))
         view.settings().set("default_dir", view.window().folders()[0])
+
+
+
+# Sublime Deselect plugin
+#
+# based on a forum post by C0D312:
+# https://www.sublimetext.com/forum/viewtopic.php?f=2&t=4716#p21219
+
+import sublime, sublime_plugin
+
+class DeselectCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        end = self.view.sel()[0].b
+        pt = sublime.Region(end, end)
+        self.view.sel().clear()
+        self.view.sel().add(pt)
